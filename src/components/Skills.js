bdaf0da3d0203194f6c7057d9d868b7ed7e78715
Aminessid <essidmohamedamin@gmail.com>
2020-08-05 00:32:03 +0000

    add costum changes v1

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,60 +1,59 @@
-// import React from "react"
-// import Title from "./Title"
-// import { FaAngleDoubleRight } from "react-icons/fa"
-// import { graphql, useStaticQuery } from "gatsby"
-// import { Link } from "gatsby"
+import React from "react"
+import Title from "./Title"
+import { graphql, useStaticQuery } from "gatsby"
+import { Link } from "gatsby"
+import Image from "gatsby-image"
 
-// const query = graphql`
-// {
-//   allStrapiSkills(sort: {fields: strapiId, order: ASC}) {
-//     nodes {
-//       company
-//       date
-//       position
-//       desc {
-//         id
-//         name
-//       }
-//     }
-//   }
-// }
-// `
+const query = graphql`
+{
+    allStrapiSkills {
+        nodes {
+          domain
+          id
+          largeImage {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          smallImage {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+}
+`
 
-// const Jobs = () => {
-//   const data = useStaticQuery(query)
-//   const {allStrapiJobs: {nodes: jobs}} = data;
-//   const [value, setValue] = React.useState(0);
-//   const {company, position, date, desc} = jobs[value]; 
-//   return <section className="section jobs">
-//     <Title title='experience'/>
-//     <div className="jobs-center">
-//       <div className="btn-container">
-//         {jobs.map((item, index) => {
-//           return (<button 
-//               key={index} 
-//               onClick={()=> setValue(index)}
-//               className={`job-btn ${index === value && 'active-btn'}`}>
-//                 {item.company}
-//             </button>)
-//           })}
-//       </div>
-//       <article className='job-info'>   
-//         <h3>{position}</h3>
-//         <h4>{company}</h4>
-//         <p className="job-date">{date}</p>
-//         {
-//           desc.map(item => {
-//             return <div key={item.id} className='job-desc'>
-//               <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
-//               {item.name}
-//             </div>
-//           })
-//         }
-//       </article>
-//     </div>
-//     <Link to='/about' className='btn center-btn'>
-//     more info</Link>
-//   </section>
-// }
+const Skills = () => {
+  const data = useStaticQuery(query)
+  const {allStrapiSkills: {nodes: skills}} = data;
+  const [value, setValue] = React.useState(0);
+  const {largeImage} = skills[value]; 
+  return <section className="section">
+    <Title title='skills'/>
+    <div className="skills-center">
+      <div className="skill-container">
+        {skills.map((item, index) => {
+          return (<button 
+              key={index} 
+              onClick={()=> setValue(index)}
+              className={`skill-btn ${index === value && 'active-btn'}`}>
+                {item.domain}
+            </button>)
+          })}
+      </div>
+      <article className='skill-info'>   
+        <Image fluid={largeImage.childImageSharp.fluid} className='skill-largeImage'/>
+      </article>
+    </div>
+    <Link to='/about' className='btn center-btn'>
+    more info</Link>
+  </section>
+}
 
-// export default Jobs
+export default Skills
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,20 @@ import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
 // import Jobs from "../components/Jobs"
+import Skills from "../components/Skills"
 import Projects from "../components/Projects"
 // import Blogs from "../components/Blogs"
 import SEO from '../components/SEO'
 export default ({data}) => {
   const {
-    allStrapiProjects:{nodes:projects}
+    allStrapiProjects:{nodes:projects},
   } = data
   return <Layout>
     <SEO title='Home' description='our home page'/>
     <Hero/>
     <Services/>
     {/* <Jobs/> */}
+    <Skills/>
     <Projects projects={projects} title='featured projects' showLink />
     {/* <Blogs blogs={blogs} title='latest blogs' showLink /> */}
   </Layout>
@@ -43,5 +45,6 @@ export const query = graphql`
         }
       }
     }
+
   }
 `
