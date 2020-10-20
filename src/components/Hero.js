9ce729abc938870eb55df219cec048d68e3e71d2
Aminessid <essidmohamedamin@gmail.com>
2020-10-20 22:06:16 +0100

    change hero img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,48 +3,60 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
+// ...GatsbyImageSharpFixed
 const query = graphql`
-{
-  file(relativePath: {eq: "hero-img.png"}) {
-    childImageSharp {
-      fluid {
-        ...GatsbyImageSharpFluid
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fixed(width: 600, height: 490) {
+          ...GatsbyImageSharpFixed
+        }
       }
     }
   }
-}
 `
 
 const Hero = () => {
   const {
     file: {
-      childImageSharp: {fluid}
-    }
-  } = useStaticQuery(query);
-  return <header className='hero'>
-    <div className="section-center hero-center">
-      <article className="hero-info">
-        <div>
-          <div className="underline"></div>
+      childImageSharp: { fixed },
+    },
+  } = useStaticQuery(query)
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
             <h1>I'm Ameen</h1>
             <h4>full-stack web developer and UI/UX designer</h4>
-            <p className='hero-description'>I can talk about myself all the day. But I will just repeat what Patrick Winston said : ‘Your success in life will be determined largely by your ability to speak, your ability to write, and the quality of your ideas’. <br/>
-            For the last one you can see <Link to='/projects'>my projects</Link> to be sure that I got the knowledge, the skills and the quality of ideas. And if you want to really know me, and check my ability to write you can read what i wrote <Link to ='/about'>about me</Link>. Finally if you want to test my super power of speaking and communication feel free to <Link to ='/contact'>contact me</Link>. 
+            <p className="hero-description">
+              I can talk about myself all the day. But I will just repeat what
+              Patrick Winston said : ‘Your success in life will be determined
+              largely by your ability to speak, your ability to write, and the
+              quality of your ideas’. <br />
+              For the last one you can see{" "}
+              <Link to="/projects">my projects</Link> to be sure that I got the
+              knowledge, the skills and the quality of ideas. And if you want to
+              really know me, and check my ability to write you can read what i
+              wrote <Link to="/about">about me</Link>. Finally if you want to
+              test my super power of speaking and communication feel free to{" "}
+              <Link to="/contact">contact me</Link>.
             </p>
-            <h4 style={{fontSize: "1rem"}}>Email: ameenessid@gmail.com</h4>
+            <h4 style={{ fontSize: "1rem" }}>Email: ameenessid@gmail.com</h4>
             {/* , <Link to ='/codingChalenges'>my coding challenges</Link> */}
             {/* <Link to ='/blogs'>my blogs</Link> */}
             {/* and  <a href='#'>my Github</a> */}
-            <Link  to ='/contact' className='btn'>
+            <Link to="/contact" className="btn">
               contact me
             </Link>
             <SocialLinks />
-        </div>
-      </article>
-      <Image fluid={fluid} className='hero-img' />
-    </div>
-  </header>
+          </div>
+        </article>
+        <Image fixed={fixed} className="hero-img" />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
